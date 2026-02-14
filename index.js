0b384f04ef6b52e126dec8e5b1c5de8b60f3def9
philip <philipmagar111@gmail.com>
2026-02-14 17:53:00 +0545

    day 2

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
-
+const ballRadius =10;
 let x=canvas.width/2;
 let y=canvas.height - 30;
 const dx = 2;
 const dy = -2;
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let rightPressed = false;
+let leftPressed = false;
 
+document.addEventListener("keydown", keyDownHandler);
+document.addEventListener("keyup", keyUpHandler);
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;}
+    }
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }   
+}
 
 function drawBall(){
     ctx.beginPath();
@@ -15,23 +36,43 @@ function drawBall(){
     ctx.closePath();
 }
 
+function drawPaddle(){
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#e9e9e9ff";
+    ctx.fill();
+    ctx.closePath();
+}
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-
+    drawPaddle();
     if(x+ dx>canvas.width- ballRadius|| x+dx < ballRadius){
         dx= -dx;
     }
     if(y+dy>canvas.height- ballRadius|| y+dy < ballRadius){
         dy= -dy;
     }
+    if(rightPressed){
+        paddleX += 7;
+        if (paddleX + paddleWidth > canvas.width) {
+            paddleX = canvas.width - paddleWidth;
+        }else if(leftPressed){
+            paddleX -= 7;
+            if (paddleX < 0) {
+                paddleX = 0;
+            }       
+    }
+    x= x + dx;
+    y= y + dy;
  
 }
 function startGame(){
     setInterval(draw, 10);
 }
 const runButton = document.getElementById("runButton");
-const stopButton = document.getElementById("stopButton");
-
-runButton.addEventListener("click", startGame);
-stopButton.addEventListener("click", stopGame);
+runButton.addEventListener("click", () => {
+  startGame();
+  runButton.disabled = true;
+});
+}
